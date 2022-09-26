7ec551cfa94fe83fbbd25147040685c1bf82e5b2
ashwin1111 <winash1998@gmail.com>
2022-09-26 11:10:52 +0530

    Remove admin and test files

--- fyle_partner_dashboard_api/urls.py
+++ fyle_partner_dashboard_api/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/auth/', include('fyle_rest_auth.urls')),
     path('api/partner/', include('apps.partner.urls')),
     path('api/user/', include('apps.users.urls'))
