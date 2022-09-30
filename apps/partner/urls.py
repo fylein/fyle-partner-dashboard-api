7de79d4bf90ee54ca6ff371e55408a865b81e127
ashwin1111 <winash1998@gmail.com>
2022-09-30 13:50:52 +0530

    Add ready call, along with unit test

--- apps/partner/urls.py
+++ apps/partner/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PartnerOrgView, OrgsView
+from .views import PartnerOrgView, OrgsView, ReadyView
 
 urlpatterns = [
     path('', PartnerOrgView.as_view(), name='partner_orgs'),
-    path('orgs/', OrgsView.as_view(), name='orgs')
+    path('orgs/', OrgsView.as_view(), name='orgs'),
+     path('ready/', ReadyView.as_view(), name='ready')
 ]
