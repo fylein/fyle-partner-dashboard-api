f2ab6a7789513d5063d2533a4c9e1507e75ea3fe
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-10-20 11:57:30 +0530

    Gevent API changes (#26)

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,16 +59,6 @@ def default_session_fixture(request):
     )
     patched_6.__enter__()
 
-    def unpatch():
-        patched_1.__exit__()
-        patched_2.__exit__()
-        patched_3.__exit__()
-        patched_4.__exit__()
-        patched_5.__exit__()
-        patched_6.__exit__()
-
-    request.addfinalizer(unpatch)
-
 
 @pytest.fixture()
 def access_token():
