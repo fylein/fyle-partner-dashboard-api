f19b2104469195fd05114ee664ae7eacc33640ff
ashwin1111 <winash1998@gmail.com>
2025-10-27 11:48:06 +0530

    chore: enable rebranding by default

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -13,7 +13,7 @@ class PartnerOrg(models.Model):
     name = models.CharField(max_length=255, help_text='Name of the partner primary org')
     user = models.ManyToManyField(User, help_text='Reference to users table')
     primary_org_id = models.CharField(max_length=255, help_text='Org Id', unique=True)
-    is_org_rebranded = models.BooleanField(default=False, help_text='Is Org Rebranded')
+    is_org_rebranded = models.BooleanField(default=True, help_text='Is Org Rebranded')
     currency = models.CharField(max_length=5, help_text='Fyle Currency', null=True)
     cluster_domain = models.CharField(max_length=255, help_text='Fyle Cluster domain')
     created_at = models.DateTimeField(auto_now_add=True, help_text='Created at datetime')
